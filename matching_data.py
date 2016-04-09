8bdf5123f2d5de5c3c585ac214368e8bbe36c1cc
Mengye Ren <renmengye@gmail.com>
2016-04-09 16:51:52 -0400

    matching data completed

--- matching_data.py
+++ matching_data.py
@@ -3,6 +3,7 @@ import numpy as np
 import sharded_hdf5 as sh
 import os
 import cv2
+import progress_bar as pb
 
 log = logger.get()
 
@@ -46,27 +47,31 @@ def crop_patch(image, bbox, patch_size, padding, padding_noise, center_noise,
 
 
 def get_dataset(folder, opt, split='train', seqs=None):
-    """Get matching dataset.
+    """Get matching dataset. 
     
     Args:
 
         folder: folder where the dataset is
 
-        opt:
-            patch_height:
-            patch_width:
-            center_noise:
-            padding_noise:
-            padding_mean:
-            num_ex_pos:
-            num_ex_neg:
+        opt: dict
+            patch_height: height of the extracted patch
+            patch_width: width of the extracted patch
+            center_noise: +/- noise of center shift (uniform), relative to size
+            padding_noise: +/- noise of padding (uniform), relative to size
+            padding_mean: mean of padding
+            num_ex_pos: number of positive examples per sequence
+            num_ex_neg: number of negative examples per sequence
+            shuffle: shuffle the final dataset
 
-        split:
-            train: sequences 0 - 12
-            valid: sequences 13 - 20
+        split: string, 'train': sequences 0 - 12, 'valid': sequences 13 - 20
 
-        seqs:
-            list of sequences. if none,
+        seqs: list of sequences.
+
+    Returns:
+        dataset: dict
+            images_0: [B, H, W, 3], first instance patches
+            images_1: [B, H, W, 3], second instance patches
+            label: [B], 1/0, whether they are the same instance. 
 
     """
     patch_height = opt['patch_height']
@@ -76,6 +81,7 @@ def get_dataset(folder, opt, split='train', seqs=None):
     padding_mean = opt['padding_mean']
     num_ex_pos = opt['num_ex_pos']
     num_ex_neg = opt['num_ex_neg']
+    shuffle = opt['shuffle']
 
     dataset_pattern = os.path.join(folder, 'dataset-*')
     dataset_file = sh.ShardedFile.from_pattern_read(dataset_pattern)
@@ -90,18 +96,19 @@ def get_dataset(folder, opt, split='train', seqs=None):
             seqs = range(13, 21)
         else:
             raise Exception('Unknown split: {}'.format(split))
+        pass
 
     with sh.ShardedFileReader(dataset_file) as reader:
-        for seq_num in seqs:
+        for seq_num in pb.get_iter(seqs):
             seq_data = reader[seq_num]
-            log.error(seq_num)
             images = seq_data['images_0']
             gt_bbox = seq_data['gt_bbox']
             num_obj = gt_bbox.shape[0]
             num_frames = gt_bbox.shape[1]
             output_images = np.zeros([num_ex_neg + num_ex_pos,
-                                      2, patch_height, patch_width, 3])
-            output_labels = np.zeros([num_ex_neg + num_ex_pos])
+                                      2, patch_height, patch_width, 3],
+                                     dtype='uint8')
+            output_labels = np.zeros([num_ex_neg + num_ex_pos], dtype='uint8')
             dataset_images.append(output_images)
             dataset_labels.append(output_labels)
 
@@ -111,6 +118,7 @@ def get_dataset(folder, opt, split='train', seqs=None):
                 while obj_id1 == obj_id2:
                     obj_id1 = int(np.floor(random.uniform(0, num_obj)))
                     obj_id2 = int(np.floor(random.uniform(0, num_obj)))
+                    pass
 
                 non_zero_frames1 = gt_bbox[obj_id1, :, 4].nonzero()[0]
                 idx1 = np.floor(random.uniform(0,
@@ -133,19 +141,22 @@ def get_dataset(folder, opt, split='train', seqs=None):
                 output_images[ii, 1] = crop_patch(
                     image2, bbox2, patch_size, padding_mean, padding_noise,
                     center_noise, random)
-                output_labels[ii] = 0.0
+                output_labels[ii] = 0
+                pass
 
             for ii in xrange(num_ex_pos):
                 frames = np.array([0])
                 while frames.shape[0] <= 1:
                     obj_id = int(np.floor(random.uniform(0, num_obj)))
                     frames = gt_bbox[obj_id, :, 4].nonzero()[0]
+                    pass
 
                 idx1 = 0
                 idx2 = 0
                 while idx1 == idx2:
                     idx1 = int(np.floor(random.uniform(0, frames.shape[0])))
                     idx2 = int(np.floor(random.uniform(0, frames.shape[0])))
+                    pass
 
                 frm1 = frames[idx1]
                 frm2 = frames[idx2]
@@ -164,10 +175,44 @@ def get_dataset(folder, opt, split='train', seqs=None):
                 output_images[jj, 1] = crop_patch(
                     image2, bbox2, patch_size, padding_mean, padding_noise,
                     center_noise, random)
-                output_labels[jj] = 1.0
+                output_labels[jj] = 1
+                pass
+            pass
         pass
 
-    pass
+    num_ex = 0
+    for ss in xrange(len(seqs)):
+        num_ex += dataset_images[ss].shape[0]
+        pass
+
+    final_images = np.zeros([num_ex, 2, patch_height, patch_width, 3],
+                            dtype='uint8')
+    final_labels = np.zeros([num_ex], dtype='uint8')
+    log.info('Image shape: {}'.format(final_images.shape))
+    log.info('Label shape: {}'.format(final_labels.shape))
+
+    counter = 0
+    for ss in xrange(len(seqs)):
+        _num_ex = dataset_images[ss].shape[0]
+        final_images[counter: counter + _num_ex] = dataset_images[ss]
+        final_labels[counter: counter + _num_ex] = dataset_labels[ss]
+        counter += _num_ex
+        pass
+
+    if shuffle:
+        idx = np.arange(num_ex)
+        random.shuffle(idx)
+        final_images = final_images[idx]
+        final_labels = final_labels[idx]
+        pass
+
+    dataset = {
+        'images_0': final_images[:, 0],
+        'images_1': final_images[:, 1],
+        'labels': final_labels
+    }
+
+    return dataset
 
 
 if __name__ == '__main__':
@@ -178,7 +223,11 @@ if __name__ == '__main__':
         'padding_noise': 0.2,
         'padding_mean': 0.2,
         'num_ex_pos': 100,
-        'num_ex_neg': 100
+        'num_ex_neg': 100,
+        'shuffle': True
     }
 
-    get_dataset('/ais/gobi3/u/mren/data/kitti/tracking/training', opt, 'train')
+    d = get_dataset(
+        '/ais/gobi3/u/mren/data/kitti/tracking/training', opt, 'train')
+
+    print d
