a432133597feb6767e585dadd97e2b213e1f1ae4
Renjie Liao <rjliao@guppy7.ais.sandbox>
2016-04-11 17:16:36 -0400

    indent change from tab to space

--- build_deep_tracker.py
+++ build_deep_tracker.py
@@ -5,146 +5,146 @@ import numpy as np
 from grad_clip_optim import GradientClipOptimizer
 
 def get_device_fn(device):
-	"""Choose device for different ops."""
-	OPS_ON_CPU = set(['ResizeBilinear', 'ResizeBilinearGrad', 'Mod', 'CumMin', 'CumMinGrad', 'Hungarian', 'Reverse', 'SparseToDense', 'BatchMatMul'])
+    """Choose device for different ops."""
+    OPS_ON_CPU = set(['ResizeBilinear', 'ResizeBilinearGrad', 'Mod', 'CumMin', 'CumMinGrad', 'Hungarian', 'Reverse', 'SparseToDense', 'BatchMatMul'])
 
-	def _device_fn(op):
-		if op.type in OPS_ON_CPU:
-			return "/cpu:0"
-		else:
-			# Other ops will be placed on GPU if available, otherwise CPU.
-			return device
+    def _device_fn(op):
+        if op.type in OPS_ON_CPU:
+            return "/cpu:0"
+        else:
+            # Other ops will be placed on GPU if available, otherwise CPU.
+            return device
 
-	return _device_fn
+    return _device_fn
 
 def compute_IOU(bboxA, bboxB):
-	"""Compute the Intersection Over Union.
-	Args:
-		bboxA: [n, 4] tensor; order is left, top, right, bottom
-		bboxB: [n, 4] tensor
+    """Compute the Intersection Over Union.
+    Args:
+        bboxA: [n, 4] tensor; order is left, top, right, bottom
+        bboxB: [n, 4] tensor
 
-	Return:
-		IOU: [n, 1]
-	"""
+    Return:
+        IOU: [n, 1]
+    """
 
-	x1A, y1A, x2A, y2A = tf.split(1, 4, bboxA)
-	x1B, y1B, x2B, y2B = tf.split(1, 4, bboxB)
+    x1A, y1A, x2A, y2A = tf.split(1, 4, bboxA)
+    x1B, y1B, x2B, y2B = tf.split(1, 4, bboxB)
 
-	# compute intersection
-	x1_max = tf.maximum(x1A, x1B)
-	y1_max = tf.maximum(y1A, y1B)
-	x2_min = tf.minimum(x2A, x2B)
-	y2_min = tf.minimum(y2A, y2B)
+    # compute intersection
+    x1_max = tf.maximum(x1A, x1B)
+    y1_max = tf.maximum(y1A, y1B)
+    x2_min = tf.minimum(x2A, x2B)
+    y2_min = tf.minimum(y2A, y2B)
 
-	overlap_flag = tf.logical_and( tf.less(x1_max, x2_min), tf.less(y1_max, y2_min) )
-	overlap_area = tf.mul(tf.to_float(overlap_flag), tf.mul( x2_min - x1_max, y2_min - y1_max ) )
+    overlap_flag = tf.logical_and( tf.less(x1_max, x2_min), tf.less(y1_max, y2_min) )
+    overlap_area = tf.mul(tf.to_float(overlap_flag), tf.mul( x2_min - x1_max, y2_min - y1_max ) )
 
-	# compute union
-	areaA = tf.mul( x2A - x1A, y2A - y1A )
-	areaB = tf.mul( x2B - x1B, y2B - y1B )
-	union_area = areaA + areaB - overlap_area
+    # compute union
+    areaA = tf.mul( x2A - x1A, y2A - y1A )
+    areaB = tf.mul( x2B - x1B, y2B - y1B )
+    union_area = areaA + areaB - overlap_area
 
-	return tf.div(overlap_area, union_area)
+    return tf.div(overlap_area, union_area)
 
 def build_tracking_model(opt, device='/cpu:0'):
-	model = {}
-	
-	batch_size = opt['batch_size']
-	cnn_filter_size = opt['cnn_filter_size']
-	cnn_num_channel = opt['cnn_num_channel']
-	cnn_pool_size = opt['cnn_pool_size']
-	img_num_channel = opt['img_channel']
-	use_bn = opt['use_batch_norm']	
-	height = opt['img_height']
-	width = opt['img_width']
-	weight_decay = opt['weight_decay']
-	rnn_hidden_dim = opt['rnn_hidden_dim']
-	mlp_hidden_dim = opt['mlp_hidden_dim']
-	base_learn_rate = opt['base_learn_rate']
-	learn_rate_decay_step = opt['learn_rate_decay_step']
-	learn_rate_decay_rate = opt['learn_rate_decay_rate']
+    model = {}
+    
+    batch_size = opt['batch_size']
+    cnn_filter_size = opt['cnn_filter_size']
+    cnn_num_channel = opt['cnn_num_channel']
+    cnn_pool_size = opt['cnn_pool_size']
+    img_num_channel = opt['img_channel']
+    use_bn = opt['use_batch_norm']  
+    height = opt['img_height']
+    width = opt['img_width']
+    weight_decay = opt['weight_decay']
+    rnn_hidden_dim = opt['rnn_hidden_dim']
+    mlp_hidden_dim = opt['mlp_hidden_dim']
+    base_learn_rate = opt['base_learn_rate']
+    learn_rate_decay_step = opt['learn_rate_decay_step']
+    learn_rate_decay_rate = opt['learn_rate_decay_rate']
 
-	with tf.device(get_device_fn(device)):
-		phase_train = tf.placeholder('bool')
-		imgs = tf.placeholder(tf.float32, [batch_size, height, width, img_num_channel])
-		init_bbox = tf.placeholder(tf.float32, [1, 4])
-		gt_bbox = tf.placeholder(tf.float32, [batch_size, 4])
-		gt_score = tf.placeholder(tf.float32, [batch_size, 1])
+    with tf.device(get_device_fn(device)):
+        phase_train = tf.placeholder('bool')
+        imgs = tf.placeholder(tf.float32, [batch_size, height, width, img_num_channel])
+        init_bbox = tf.placeholder(tf.float32, [1, 4])
+        gt_bbox = tf.placeholder(tf.float32, [batch_size, 4])
+        gt_score = tf.placeholder(tf.float32, [batch_size, 1])
 
-		model['imgs'] = imgs
-		model['gt_bbox'] = gt_bbox
-		model['gt_score'] = gt_score
-		model['init_bbox'] = init_bbox
-		model['phase_train'] = phase_train
+        model['imgs'] = imgs
+        model['gt_bbox'] = gt_bbox
+        model['gt_score'] = gt_score
+        model['init_bbox'] = init_bbox
+        model['phase_train'] = phase_train
 
-		# define a CNN model
-		cnn_filter = cnn_filter_size
-		cnn_nlayer = len(cnn_filter)
-		cnn_channel = [img_num_channel] + cnn_num_channel
-		cnn_pool = cnn_pool_size
-		cnn_act = [tf.nn.relu] * cnn_nlayer
-		cnn_use_bn = [use_bn] * cnn_nlayer
+        # define a CNN model
+        cnn_filter = cnn_filter_size
+        cnn_nlayer = len(cnn_filter)
+        cnn_channel = [img_num_channel] + cnn_num_channel
+        cnn_pool = cnn_pool_size
+        cnn_act = [tf.nn.relu] * cnn_nlayer
+        cnn_use_bn = [use_bn] * cnn_nlayer
 
-		cnn_model = nn.cnn(cnn_filter, cnn_channel, cnn_pool, cnn_act,
-		              cnn_use_bn, phase_train=phase_train, wd=weight_decay)
-		
-		h_cnn = cnn_model(imgs)	# h_cnn is a list and stores the output of every layer
-		cnn_output = h_cnn[-1]
-		model['cnn_output'] = cnn_output
+        cnn_model = nn.cnn(cnn_filter, cnn_channel, cnn_pool, cnn_act,
+                      cnn_use_bn, phase_train=phase_train, wd=weight_decay)
+        
+        h_cnn = cnn_model(imgs) # h_cnn is a list and stores the output of every layer
+        cnn_output = h_cnn[-1]
+        model['cnn_output'] = cnn_output
 
-		# define a RNN(LSTM) model
-		cnn_subsample = np.array(cnn_pool).prod()
-		rnn_h = height / cnn_subsample
-		rnn_w = width / cnn_subsample
-		rnn_dim = cnn_channel[-1]	    
-		rnn_inp_dim = rnn_h * rnn_w * rnn_dim
+        # define a RNN(LSTM) model
+        cnn_subsample = np.array(cnn_pool).prod()
+        rnn_h = height / cnn_subsample
+        rnn_w = width / cnn_subsample
+        rnn_dim = cnn_channel[-1]       
+        rnn_inp_dim = rnn_h * rnn_w * rnn_dim
 
-		# define a linear mapping: initial bbox -> hidden state
-		W_bbox = tf.Variable(tf.truncated_normal([rnn_hidden_dim, 4], stddev=0.01))
-		W_score = tf.Variable(tf.truncated_normal([rnn_hidden_dim, 1], stddev=0.01))
+        # define a linear mapping: initial bbox -> hidden state
+        W_bbox = tf.Variable(tf.truncated_normal([rnn_hidden_dim, 4], stddev=0.01))
+        W_score = tf.Variable(tf.truncated_normal([rnn_hidden_dim, 1], stddev=0.01))
 
-		rnn_state = [None] * (batch_size + 1)
-		rnn_state[-1] = tf.concat(1, [tf.zeros([1, rnn_hidden_dim], tf.float32), tf.matmul(init_bbox, W_bbox, False, True)])
-		rnn_hidden_feat = [None] * batch_size
-		predict_bbox = tf.zeros([batch_size, 4])
-		predict_score = tf.zeros([batch_size, 1])
-		IOU_score = [None] * batch_size
+        rnn_state = [None] * (batch_size + 1)
+        rnn_state[-1] = tf.concat(1, [tf.zeros([1, rnn_hidden_dim], tf.float32), tf.matmul(init_bbox, W_bbox, False, True)])
+        rnn_hidden_feat = [None] * batch_size
+        predict_bbox = tf.zeros([batch_size, 4])
+        predict_score = tf.zeros([batch_size, 1])
+        IOU_score = [None] * batch_size
 
-		rnn_cell = nn.lstm(rnn_inp_dim, rnn_hidden_dim, wd=weight_decay)
+        rnn_cell = nn.lstm(rnn_inp_dim, rnn_hidden_dim, wd=weight_decay)
 
-		cnn_feat = tf.split(0, batch_size, cnn_output)
+        cnn_feat = tf.split(0, batch_size, cnn_output)
 
-		for tt in xrange(batch_size):
-			cnn_feat[tt] = tf.reshape(cnn_feat[tt], [1, rnn_inp_dim])
-			rnn_state[tt], _, _, _ = rnn_cell(cnn_feat[tt], rnn_state[tt - 1])
-			rnn_hidden_feat[tt] = tf.slice(rnn_state[tt], [0, rnn_hidden_dim], [-1, rnn_hidden_dim])
-			
-		predict_bbox = tf.matmul(tf.concat(0, rnn_hidden_feat), W_bbox)
-		predict_score = tf.matmul(tf.concat(0, rnn_hidden_feat), W_score)
+        for tt in xrange(batch_size):
+            cnn_feat[tt] = tf.reshape(cnn_feat[tt], [1, rnn_inp_dim])
+            rnn_state[tt], _, _, _ = rnn_cell(cnn_feat[tt], rnn_state[tt - 1])
+            rnn_hidden_feat[tt] = tf.slice(rnn_state[tt], [0, rnn_hidden_dim], [-1, rnn_hidden_dim])
+            
+        predict_bbox = tf.matmul(tf.concat(0, rnn_hidden_feat), W_bbox)
+        predict_score = tf.matmul(tf.concat(0, rnn_hidden_feat), W_score)
 
-		IOU_score = compute_IOU(predict_bbox, gt_bbox)
+        IOU_score = compute_IOU(predict_bbox, gt_bbox)
 
-		model['predict_bbox'] = predict_bbox
-		model['predict_score'] = predict_score
+        model['predict_bbox'] = predict_bbox
+        model['predict_score'] = predict_score
 
-		# IOU loss + cross-entropy loss
-		IOU_loss = tf.reduce_sum(gt_score * (1 - IOU_score))
-		cross_entropy = -tf.reduce_sum(gt_score * tf.log(predict_score))
+        # IOU loss + cross-entropy loss
+        IOU_loss = tf.reduce_sum(gt_score * (- IOU_score))
+        cross_entropy = -tf.reduce_sum(gt_score * tf.log(predict_score))
 
-		model['IOU_loss'] = IOU_loss
-		model['CE_loss'] = cross_entropy
-		
-		global_step = tf.Variable(0.0)
-		eps = 1e-7
+        model['IOU_loss'] = IOU_loss
+        model['CE_loss'] = cross_entropy
+        
+        global_step = tf.Variable(0.0)
+        eps = 1e-7
 
-		learn_rate = tf.train.exponential_decay(
-			base_learn_rate, global_step, learn_rate_decay_step,
-			learn_rate_decay_rate, staircase=True)
-		model['learn_rate'] = learn_rate
+        learn_rate = tf.train.exponential_decay(
+            base_learn_rate, global_step, learn_rate_decay_step,
+            learn_rate_decay_rate, staircase=True)
+        model['learn_rate'] = learn_rate
 
-		train_step = GradientClipOptimizer(
-			tf.train.AdamOptimizer(learn_rate, epsilon=eps),
-			clip=1.0).minimize(IOU_loss + cross_entropy, global_step=global_step)
-		model['train_step'] = train_step
+        train_step = GradientClipOptimizer(
+            tf.train.AdamOptimizer(learn_rate, epsilon=eps),
+            clip=1.0).minimize(IOU_loss + cross_entropy, global_step=global_step)
+        model['train_step'] = train_step
 
-	return model
+    return model
--- deep_tracker_train.py
+++ deep_tracker_train.py
@@ -17,143 +17,143 @@ from build_deep_tracker import compute_IOU
 from kitti import get_dataset
 
 def next_batch(imgs, labels, scores, idx_sample, batch_size, num_train):
-	if idx_sample >= num_train:
-		raise NameError('Incorrect index of sample')
-	
-	current_batch_img = []
-	current_batch_label = []
-	current_batch_score = []
+    if idx_sample >= num_train:
+        raise NameError('Incorrect index of sample')
+    
+    current_batch_img = []
+    current_batch_label = []
+    current_batch_score = []
 
-	if idx_sample + batch_size > num_train:
-		current_batch_img = [ imgs[idx_sample : num_train], imgs[0 : batch_size - (num_train - idx_sample)] ]
-		current_batch_label = [ labels[idx_sample : num_train], labels[0 : batch_size - (num_train - idx_sample)] ]
-		current_batch_score = [ scores[idx_sample : num_train], scores[0 : batch_size - (num_train - idx_sample)] ]
-	else:
-		current_batch_img = imgs[idx_sample : idx_sample + batch_size]
-		current_batch_label = labels[idx_sample : idx_sample + batch_size]
-		current_batch_score = scores[idx_sample : idx_sample + batch_size]
+    if idx_sample + batch_size > num_train:
+        current_batch_img = [ imgs[idx_sample : num_train], imgs[0 : batch_size - (num_train - idx_sample)] ]
+        current_batch_label = [ labels[idx_sample : num_train], labels[0 : batch_size - (num_train - idx_sample)] ]
+        current_batch_score = [ scores[idx_sample : num_train], scores[0 : batch_size - (num_train - idx_sample)] ]
+    else:
+        current_batch_img = imgs[idx_sample : idx_sample + batch_size]
+        current_batch_label = labels[idx_sample : idx_sample + batch_size]
+        current_batch_score = scores[idx_sample : idx_sample + batch_size]
 
-	return current_batch_img, current_batch_label, current_batch_score
+    return current_batch_img, current_batch_label, current_batch_score
 
 if __name__ == "__main__":
 
-	# folder = '/ais/gobi4/rjliao/Projects/CSC2541/data/TUD/cvpr10_tud_stadtmitte'
-	folder = '/ais/gobi3/u/mren/data/kitti/tracking/'
-	device = '/gpu:3'
-	
-	max_iter = 1000	
-	batch_size = 10  	# sequence length for training
-	display_iter = 10
-	snapshot_iter = 1000
-	height = 128
-	width = 448
+    # folder = '/ais/gobi4/rjliao/Projects/CSC2541/data/TUD/cvpr10_tud_stadtmitte'
+    folder = '/ais/gobi3/u/mren/data/kitti/tracking/'
+    device = '/gpu:3'
+    
+    max_iter = 1000 
+    batch_size = 10     # sequence length for training
+    display_iter = 10
+    snapshot_iter = 1000
+    height = 128
+    width = 448
 
-	# read data
-	# dataset = get_dataset(folder)
-	dataset = get_dataset(folder, 'train')
+    # read data
+    # dataset = get_dataset(folder)
+    dataset = get_dataset(folder, 'train')
 
-	# setting model
-	opt_tracking = {}
-	opt_tracking['batch_size'] = batch_size
-	opt_tracking['cnn_filter_size'] = [5, 5, 5]
-	opt_tracking['cnn_num_channel'] = [64, 64, 32]
-	opt_tracking['cnn_pool_size'] = [2, 2, 2]
-	opt_tracking['img_channel'] = 3
-	opt_tracking['use_batch_norm']= True
-	opt_tracking['img_height'] = height
-	opt_tracking['img_width'] = width
-	opt_tracking['weight_decay'] = 1.0e-5
-	opt_tracking['rnn_hidden_dim'] = 100
-	opt_tracking['mlp_hidden_dim'] = [100, 100]
-	opt_tracking['base_learn_rate'] = 1.0e-3
-	opt_tracking['learn_rate_decay_step'] = 10000
-	opt_tracking['learn_rate_decay_rate'] = 0.96
-	
-	tracking_model = build_tracking_model(opt_tracking, device)
+    # setting model
+    opt_tracking = {}
+    opt_tracking['batch_size'] = batch_size
+    opt_tracking['cnn_filter_size'] = [5, 5, 5]
+    opt_tracking['cnn_num_channel'] = [64, 64, 32]
+    opt_tracking['cnn_pool_size'] = [2, 2, 2]
+    opt_tracking['img_channel'] = 3
+    opt_tracking['use_batch_norm']= True
+    opt_tracking['img_height'] = height
+    opt_tracking['img_width'] = width
+    opt_tracking['weight_decay'] = 1.0e-5
+    opt_tracking['rnn_hidden_dim'] = 100
+    opt_tracking['mlp_hidden_dim'] = [100, 100]
+    opt_tracking['base_learn_rate'] = 1.0e-3
+    opt_tracking['learn_rate_decay_step'] = 10000
+    opt_tracking['learn_rate_decay_rate'] = 0.96
+    
+    tracking_model = build_tracking_model(opt_tracking, device)
 
-	sess = tf.Session()
-	sess.run(tf.initialize_all_variables())
-	saver = tf.train.Saver()
-	nodes_run = ['train_step', 'IOU_loss', 'CE_loss', 'predict_bbox', 'predict_score']
-	# nodes_run = ['predict_bbox', 'predict_score']
+    sess = tf.Session()
+    sess.run(tf.initialize_all_variables())
+    saver = tf.train.Saver()
+    nodes_run = ['train_step', 'IOU_loss', 'CE_loss', 'predict_bbox', 'predict_score']
+    # nodes_run = ['predict_bbox', 'predict_score']
 
-	with sh.ShardedFileReader(dataset) as reader:		
-		# training 
-		global_train_step = 0
+    with sh.ShardedFileReader(dataset) as reader:       
+        # training 
+        global_train_step = 0
 
-		for epoch_step in xrange(max_iter):
+        for epoch_step in xrange(max_iter):
 
-			for seq_num in xrange(len(reader)):
+            for seq_num in xrange(len(reader)):
 
-				seq_data = reader[seq_num]
-				raw_imgs = seq_data['images_0']
-				gt_bbox = seq_data['gt_bbox']
-				
-				num_obj = gt_bbox.shape[0]
-				num_imgs = raw_imgs.shape[0]
+                seq_data = reader[seq_num]
+                raw_imgs = seq_data['images_0']
+                gt_bbox = seq_data['gt_bbox']
+                
+                num_obj = gt_bbox.shape[0]
+                num_imgs = raw_imgs.shape[0]
 
-				if num_obj < 1:
-					continue
+                if num_obj < 1:
+                    continue
 
-				for obj in xrange(num_obj):
-					print "Epoch = %04d || Training %04d-th object in %04d-th sequence" % (epoch_step, obj+1, seq_num+1)
+                for obj in xrange(num_obj):
+                    print "Epoch = %04d || Training %04d-th object in %04d-th sequence" % (epoch_step, obj+1, seq_num+1)
 
-					# prepare input and output
-					train_imgs = []
-					train_gt_box = []
-					train_gt_score = []
-					skip_flag = True
-					
-					for ii in xrange(num_imgs):				
-						# gt_bbox = [left top right bottom flag]
-						if gt_bbox[obj, ii, 4] == 1:
-							# if object disappears, then using the last visible bbox for training
-							skip_flag = False
+                    # prepare input and output
+                    train_imgs = []
+                    train_gt_box = []
+                    train_gt_score = []
+                    skip_flag = True
+                    
+                    for ii in xrange(num_imgs):             
+                        # gt_bbox = [left top right bottom flag]
+                        if gt_bbox[obj, ii, 4] == 1:
+                            # if object disappears, then using the last visible bbox for training
+                            skip_flag = False
 
-						if skip_flag == False:
-							# extract raw image as input
-							train_imgs.append(cv2.resize(raw_imgs[ii, :, :], (width, height), interpolation = cv2.INTER_CUBIC))
+                        if skip_flag == False:
+                            # extract raw image as input
+                            train_imgs.append(cv2.resize(raw_imgs[ii, :, :], (width, height), interpolation = cv2.INTER_CUBIC))
 
-							# extract bbox and score as output
-							tmp_box = deepcopy(gt_bbox[obj, ii, 0:4])
-							tmp_box[0] = tmp_box[0] / raw_imgs.shape[2]
-							tmp_box[1] = tmp_box[1] / raw_imgs.shape[1]
-							tmp_box[2] = tmp_box[2] / raw_imgs.shape[2]
-							tmp_box[3] = tmp_box[3] / raw_imgs.shape[1]
-							train_gt_box.append(tmp_box)
+                            # extract bbox and score as output
+                            tmp_box = deepcopy(gt_bbox[obj, ii, 0:4])
+                            tmp_box[0] = tmp_box[0] / raw_imgs.shape[2]
+                            tmp_box[1] = tmp_box[1] / raw_imgs.shape[1]
+                            tmp_box[2] = tmp_box[2] / raw_imgs.shape[2]
+                            tmp_box[3] = tmp_box[3] / raw_imgs.shape[1]
+                            train_gt_box.append(tmp_box)
 
-							train_gt_score.append(gt_bbox[obj, ii, 4])
+                            train_gt_score.append(gt_bbox[obj, ii, 4])
 
-					# training for current sequence
-					num_train_imgs = len(train_imgs)
-					inner_max_iter = int(np.floor(num_train_imgs / batch_size))
+                    # training for current sequence
+                    num_train_imgs = len(train_imgs)
+                    inner_max_iter = int(np.floor(num_train_imgs / batch_size))
 
-					for step in xrange(inner_max_iter):
-						idx_start = (step * batch_size) % num_train_imgs
-						batch_img, batch_box, batch_score = next_batch(train_imgs, train_gt_box, train_gt_score, idx_start, batch_size, num_train_imgs)
+                    for step in xrange(inner_max_iter):
+                        idx_start = (step * batch_size) % num_train_imgs
+                        batch_img, batch_box, batch_score = next_batch(train_imgs, train_gt_box, train_gt_score, idx_start, batch_size, num_train_imgs)
 
-						node_list = [tracking_model[i] for i in nodes_run]
-						feed_data = {tracking_model['imgs']: batch_img, 
-									 tracking_model['init_bbox']: [batch_box[0]], 
-									 tracking_model['gt_bbox']: batch_box, 
-									 tracking_model['gt_score']: zip(*[batch_score]), 
-									 tracking_model['phase_train']: True}
+                        node_list = [tracking_model[i] for i in nodes_run]
+                        feed_data = {tracking_model['imgs']: batch_img, 
+                                     tracking_model['init_bbox']: [batch_box[0]], 
+                                     tracking_model['gt_bbox']: batch_box, 
+                                     tracking_model['gt_score']: zip(*[batch_score]), 
+                                     tracking_model['phase_train']: True}
 
-						results = sess.run(node_list, feed_dict=feed_data)
+                        results = sess.run(node_list, feed_dict=feed_data)
 
-						results_dict = {}
-						for rr, name in zip(results, nodes_run):
-							results_dict[name] = rr
+                        results_dict = {}
+                        for rr, name in zip(results, nodes_run):
+                            results_dict[name] = rr
 
-						# print results_dict['predict_bbox']
-						# print results_dict['predict_score']
+                        # print results_dict['predict_bbox']
+                        # print results_dict['predict_score']
 
-						if step % display_iter == 0:
-							print "Train Iter = %06d || Loss = %e" % (global_train_step+1, results_dict['IOU_loss'])
+                        if step % display_iter == 0:
+                            print "Train Iter = %06d || Loss = %e" % (global_train_step+1, results_dict['IOU_loss'])
 
-						if (global_train_step+1) % snapshot_iter == 0:
-							saver.save(sess, 'my_deep_tracker.ckpt', global_step=step)
+                        if (global_train_step+1) % snapshot_iter == 0:
+                            saver.save(sess, 'my_deep_tracker.ckpt', global_step=step)
 
-						global_train_step += 1
+                        global_train_step += 1
 
-	sess.close()
+    sess.close()
