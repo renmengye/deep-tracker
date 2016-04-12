ac5dcfa4ff3967c54c3e587308e704e0888de381
Mengye Ren <renmengye@gmail.com>
2016-04-11 21:47:43 -0400

    plot

--- run_matching.py
+++ run_matching.py
@@ -1,10 +1,11 @@
 import cslab_environ
 
-import logger
 from saver import Saver
+import logger
+import numpy as np
 import sys
 import tensorflow as tf
-import numpy as np
+import plot_utils as pu
 
 import matching_model as model
 import matching_data as data
@@ -36,6 +37,32 @@ def get_batch_fn(dataset):
     return get_batch
 
 
+def plot_output(fname, x1, x2, y_gt, y_out):
+    num_ex = y_out.shape[0]
+    num_items = 2
+    num_row, num_col, calc = pu.calc_row_col(
+        num_ex, num_items, max_items_per_row=9)
+
+    f1, axarr = plt.subplots(num_row, num_col, figsize=(10, num_row))
+    pu.set_axis_off(axarr, num_row, num_col)
+
+    for ii in xrange(num_ex):
+        for jj in xrange(num_items):
+            row, col = calc(ii, jj)
+            if jj == 0:
+                axarr[row, col].imshow(x1[ii])
+            else:
+                axarr[row, col].imshow(x2[ii])
+
+            axarr[row, col].text(0, 0, '{:.2f} {:.2f}'.format(
+                y_gt[ii], y_out[ii]),
+                color=(0, 0, 0), size=8)
+
+    plt.tight_layout(pad=2.0, w_pad=0.0, h_pad=0.0)
+    plt.savefig(fname, dpi=150)
+    plt.close('all')
+
+
 def preprocess(x1, x2, y):
     """Preprocess training data."""
     return (x1.astype('float32') / 255,
@@ -64,10 +91,12 @@ if __name__ == '__main__':
 
     idx = np.arange(10)
     get_batch = get_batch_fn(dataset)
-    x1, x2, y = get_batch(idx)
+    x1, x2, y_gt = get_batch(idx)
 
     y_out = sess.run(m['y_out'],
                      feed_dict={
                          m['x1']: x1, m['x2']: x2, m['phase_train']: False})
     print y_out
     print y
+
+    plot_output('/u/mren/test_matching.png', x1, x2, y_gt, y_out)
