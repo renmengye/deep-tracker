0d5c134af2bfe28137aa6aa58646c2a7aca47d0d
Mengye Ren <renmengye@gmail.com>
2016-04-16 12:54:35 -0400

    added trained model

--- run_detector_cnn.py
+++ run_detector_cnn.py
@@ -86,7 +86,11 @@ def _run_model(sess, m, names, feed_dict):
 
 def preprocess(x):
     """Preprocess training data."""
-    x = cv2.resize(x, (448, 128))
+    print x.shape
+    x_new = np.zeros([x.shape[0], 128, 448, 3])
+    for ii in xrange(x.shape[0]):
+        x_new[ii] = cv2.resize(x[ii], (448, 128))
+    print x_new.shape
     return x.astype('float32') / 255
 
 
