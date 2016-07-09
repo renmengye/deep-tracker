5c383778b576b5fb5276a6770041bac37938a864
Mengye Ren <renmengye@gmail.com>
2016-07-09 18:04:03 -0400

    kitti assembler working on it

---
+++ kitti_new.py
@@ -0,0 +1,59 @@
+from __future__ import division
+
+import cv2
+import h5py
+import numpy as np
+import tfplus
+import tfplus.utils.progress_bar as pb
+from tracking_data_assembler import TrackingDataAssembler
+
+
+class KITTITrackingDataAssembler(TrackingDataAssembler):
+
+    def __init__(self, folder, output_fname=None, split='train'):
+        self.folder = folder
+        if output_fname is None:
+            output_fname = os.path.join(folder, '{}_{}x{}.h5'.format(split))
+
+        if split == 'train' or 'valid':
+            self.left_folder = os.path.join(folder, 'training', 'image_02')
+        elif split == 'test':
+            self.left_folder = os.path.join(folder, 'testing', 'image_02')
+        else:
+            raise Exception('Unknown split "{}"'.format(split))
+
+        super(KITTITrackingDataAssembler, self).__init__(output_fname)
+        pass
+
+    def get_video_ids(self):
+        return os.listdir(self.left_folder).filter(lambda x: x.startswith('0'))
+
+    def get_frame_ids(self, vid_id):
+        raise Exception('Not implemented')
+
+    def get_frame_img(self, vid_id, frm_id):
+        raise Exception('Not implemented')
+
+    def get_obj_ids(self, vid_id):
+        raise Exception('Not implemented')
+
+    def get_obj_data(self, vid_id, frm_id, obj_id):
+        raise Exception('Not implemented')
+
+    pass
+
+
+class KITTITrackingDataProvider(tfplus.data.DataProvider):
+
+    def __init__(self):
+        pass
+    pass
+
+tfplus.data.data_provider.register('kitti_track', KITTITrackingDataProvider)
+
+
+if __name__ == '__main__':
+    assembler = KITTITrackingDataAssembler(
+        output_fname='/ais/gobi4/mren/data/kitti/tracking')
+    print assembler.get_video_ids()
+    pass
---
+++ tracking_data_assembler.py
@@ -0,0 +1,64 @@
+class TrackingDataAssembler(object):
+    """
+    video_id/frame_id/image: PNG encoded image
+    video_id/frame_id/object_id/bbox: (left, top, right, bottom)
+    video_id/frame_id/object_id/presence: 1/0
+    """
+
+    def __init__(self, output_fname):
+        self.log = tfplus.utils.logger.get()
+        self.output_fname = output_fname
+        self.log.info('Output h5 dataset: {}'.format(self.output_fname))
+        self.log.info('Reading video IDs')
+        self.vid_ids = self.get_video_ids()
+        pass
+
+    def save(self, key, data, group):
+        if key in group:
+            del group[key]
+        group[key] = data
+        pass
+
+    def encode(self, img):
+        return cv2.imencode('.png', img)[1]
+
+    def get_video_ids(self):
+        raise Exception('Not implemented')
+
+    def get_frame_ids(self, vid_id):
+        raise Exception('Not implemented')
+
+    def get_frame_img(self, vid_id, frm_id):
+        raise Exception('Not implemented')
+
+    def get_obj_ids(self, vid_id):
+        raise Exception('Not implemented')
+
+    def get_obj_data(self, vid_id, frm_id, obj_id):
+        raise Exception('Not implemented')
+
+    def assemble(self):
+        num_vid = len(self.vid_ids)
+
+        with h5py.File(self.output_fname, 'a') as h5f:
+            for idx in pb.get(num_vid):
+                vid_id = self.vid_ids[idx]
+                obj_ids = self.get_obj_ids(vid_id)
+                for frm_id in self.get_frame_ids(vid_id):
+                    frm_img = self.get_frame_img(vid_id, frm_id)
+                    obj_data = self.get_obj_data(vid_id, frm_id)
+                    img_enc = self.encode(frm_img)
+                    key = '{}/{}'.format(vid_id, frm_id)
+                    self.save(key + '/image', img_enc)
+                    for obj_id in obj_ids:
+                        obj_key = key + '/{}'.format(obj_id)
+                        obj_data = self.get_obj_data(vid_id, frm_id, obj_id)
+                        if obj_data is not None:
+                            self.save(obj_key + '/bbox', obj_data['bbox'])
+                            self.save(obj_key + '/presence',
+                                      obj_data['presence'])
+                        pass
+                    pass
+                pass
+            pass
+        pass
